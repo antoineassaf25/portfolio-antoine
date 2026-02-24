1f0ba58a30af7981c45844e47b454db140416f31
aassaf9 <aassaf9@gatech.edu>
2026-02-23 19:49:25 -0500

    Starting Site

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+  <nav className="navbar">
+    <div className="nav-links">
+        <a href="#projects">Projects</a>
+        <a href="#experience">Experience</a>
+        <a href="#contact">Contact</a>      
+    </div>
+  </nav>
+  )
+}
+
+export default NavBar
