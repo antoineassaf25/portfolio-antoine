8c4b716022dfdf0725b3edbd70b878530c23357e
aassaf9 <aassaf9@gatech.edu>
2026-02-23 20:55:19 -0500

    Website base

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,22 @@ import NavBar from './components/NavBar.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <NavBar />
+    <main style={{ paddingTop: "120px" }}>
+      <section id="home" style={{ height: "100vh" }}>
+        <h1>Home</h1>
+      </section>
+
+      <section id="projects" style={{ height: "100vh" }}>
+        <h1>Projects</h1>
+      </section>
+
+      <section id="experience" style={{ height: "100vh" }}>
+        <h1>Experience</h1>
+      </section>
+
+      <section id="contact" style={{ height: "100vh" }}>
+        <h1>Contact</h1>
+      </section>
+    </main>
   </StrictMode>,
 )
