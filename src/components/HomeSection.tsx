5f923046efeeed62674c537ec96e575fcf396a57
aassaf9 <aassaf9@gatech.edu>
2026-02-25 03:05:49 -0500

    About Me section

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,4 +1,6 @@
 import HeadShot from "./../assets/Headshot.jpg"
+import GitHubIcon from "./../assets/github.png"
+import LinkedInIcon from "./../assets/linkedin.png"
 
 function HomeSection() { 
   return (
@@ -8,6 +10,30 @@ function HomeSection() {
     Here is some text. this is about what i want to be talking about. this is what is about me. this is my intro.
     </p>
     <div className="about-row">
+        <a className="primary-button" href="#contact">
+            Contact Me 👋
+        </a>
+        <a
+        href="/Resume_Antoine.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="primary-button"
+        >
+            Download CV 📄
+        </a>
+        <a className="icon-button"
+        href="https://github.com/antoineassaf25"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+            <img src={GitHubIcon} alt="GitHub" style = {{width: "40px", height: "40px"}} />
+        </a>
+        <a className="icon-button"
+        href="https://www.linkedin.com/in/aassaf25"
+        target="_blank"
+        rel="noopener noreferrer">
+            <img src={LinkedInIcon} alt="LinkedIn" style = {{width: "40px", height: "40px"}}/>
+        </a>
     </div>
     </section>
   )
